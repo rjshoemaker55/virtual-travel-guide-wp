e87519f72648d56ef622779c869b69a898122750
rjshoemaker55 <rjshoemaker55@gmail.com>
2019-10-31 09:24:58 -0400

    trying more stuff

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
-app.get('/', (req, res) => {
-  console.log('working');
-  res.end();
+app.get('*', (req, res) => {
+  res.sendFile(path.join(path.join(__dirname + '/client/dist/index.html')));
 });
-
-// "heroku-postbuild": "cd client && yarn && yarn --only=dev --no-shrinkwrap && yarn build"
