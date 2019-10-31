ed432a04c982d3ec8f6b04683857559f2ec0c5b0
rjshoemaker55 <rjshoemaker55@gmail.com>
2019-10-31 09:28:07 -0400

    added static path to express

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
+app.use(expres.static(path.join(__dirname, '/client/dist')));
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(path.join(__dirname + '/client/dist/index.html')));
 });
