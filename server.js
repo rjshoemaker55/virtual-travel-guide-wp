fa0786f52b04f71cb2edfee4266ba154c6066e3c
rjshoemaker55 <rjshoemaker55@gmail.com>
2019-10-31 09:19:01 -0400

    more

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
 app.get('/', (req, res) => {
-  res.send('working');
+  console.log('working');
 });
 
 // "heroku-postbuild": "cd client && yarn && yarn --only=dev --no-shrinkwrap && yarn build"
