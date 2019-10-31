15658f0c3f87732e7b5bfcc4e763fcb291cf956d
rjshoemaker55 <rjshoemaker55@gmail.com>
2019-10-31 09:11:57 -0400

    added root route

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
-app.get('/express_backend', (req, res) => {
-  res.send({ express: 'Your Express backend is connected to React.' });
+app.get('/', (req, res) => {
+  res.send('working');
 });
